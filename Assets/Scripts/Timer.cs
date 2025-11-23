8db258a005684081b5299e3f50451813c70ba5d8
AfonsoOrmonde <93348169+AfonsoOrmonde@users.noreply.github.com>
2025-11-23 17:48:08 +0000

    Timer stoppage

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -9,17 +9,28 @@ public class Timer : MonoBehaviour
 
     float elapsedTime;
     public TextMeshProUGUI timerText;
+    bool keepCounting;
     void Start()
     {
+        keepCounting = true;
         elapsedTime = 0f;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(LayerMask.LayerToName(other.gameObject.layer) == "HandCollider")
+        {
+           keepCounting = false;
+        }
     }
     // Update is called once per frame
     void Update()
     {
-        elapsedTime += Time.deltaTime;
-        int hours = Mathf.FloorToInt(elapsedTime / 3600);
-        int minutes = Mathf.FloorToInt((elapsedTime % 3600) / 60);
-        int seconds = Mathf.FloorToInt( elapsedTime % 60);
-        timerText.text = string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
+        if(keepCounting){
+            elapsedTime += Time.deltaTime;
+            int hours = Mathf.FloorToInt(elapsedTime / 3600);
+            int minutes = Mathf.FloorToInt((elapsedTime % 3600) / 60);
+            int seconds = Mathf.FloorToInt( elapsedTime % 60);
+            timerText.text = string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);}
     }
 }
